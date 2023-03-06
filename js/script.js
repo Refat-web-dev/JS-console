02a33546dedf52583d7cce9e015b8e1a3ce4e7f7
main <refabatya@gmail.com>
2023-03-06 15:26:12 +0500

    homework

--- js/script.js
+++ js/script.js
@@ -1,41 +1,125 @@
-refat = {
-        age: 19,
-        hairColor: "Braun",
-        webDev: true,
-        object: {
-            something: 'something ',
-            something: 'something ',
-            something: 'something ',
-            something: 'something ',
-        }
+// Отфильтровать задачи в переменные а и b
+// Сохранить количество в ключе count
+// Сохранить сами задачи в массиве-ключе arr
+let arr = [{
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut au`tem",
+        "completed": false
     },
-    amal = {
-        footSize: 40,
-        eyeColor: "Black",
-        SMM: false,
-        objectTwo: {
-            something: 'something ',
-            something: 'something ',
-            something: 'something ',
-            something: 'something ',
-        }
-    };
-let newCopy = Object.assign({}, refat, amal);
-let allKeyses = Object.keys(newCopy);
-let allValues = Object.values(newCopy);
-let totalData = Object.assign({}, { allKeyses }, { allValues })
-console.log(totalData);
-totalData = [...allKeyses, ...allValues]
-let types = {
-    string: [],
-    number: [],
-    object: [],
-    boolean: [],
-};
-totalData.forEach((item) => {
-    types[typeof item].push(item)
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 6,
+        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 7,
+        "title": "illo expedita consequatur quia in",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 8,
+        "title": "quo adipisci enim quam ut ab",
+        "completed": true
+    },
+    {
+        "userId": 1,
+
+        "id": 9,
+        "title": "molestiae perspiciatis ipsa",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "title": "illo est ratione doloremque quia maiores aut",
+        "completed": true
+    },
+]
+let a = {
+    count: 0,
+    arrOne: []
+}
+
+let b = {
+    count: 0,
+    arrTwo: []
+}
+arr.forEach(el => {
+    let key = el.completed
+    if (key) {
+        a.arrOne.push(el.title)
+        a.count++
+    } else {
+        b.arrTwo.push(el.title)
+        b.count++
+    }
 })
-console.log(types);
+console.log(a, b);
+// refat = {
+//         age: 19,
+//         hairColor: "Braun",
+//         webDev: true,
+//         object: {
+//             something: 'something ',
+//             something: 'something ',
+//             something: 'something ',
+//             something: 'something ',
+//         }
+//     },
+//     amal = {
+//         footSize: 40,
+//         eyeColor: "Black",
+//         SMM: false,
+//         objectTwo: {
+//             something: 'something ',
+//             something: 'something ',
+//             something: 'something ',
+//             something: 'something ',
+//         }
+//     };
+// let newCopy = Object.assign({}, refat, amal);
+// let allKeyses = Object.keys(newCopy);
+// let allValues = Object.values(newCopy);
+// let totalData = Object.assign({}, { allKeyses }, { allValues })
+// console.log(totalData);
+// totalData = [...allKeyses, ...allValues]
+// let types = {
+//     string: [],
+//     number: [],
+//     object: [],
+//     boolean: [],
+// };
+// totalData.forEach((item) => types[typeof item].push(item))
+// console.log(types);
 // if (typeof(types) === "string") {
 
 // }
