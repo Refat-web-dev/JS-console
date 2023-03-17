126eab16e9f078f542082ec9dbf523087ea37b9c
main <refabatya@gmail.com>
2023-03-17 17:29:49 +0500

    fibonacciDone

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let arr = [0, 1]
-let i = +prompt('Enter a num')
-for (let index = 0; arr.length <= i - 1; index++) {
+let i
+do {
+    i = +prompt('Enter a num')
+} while (!Number(i));
+for (let index = 0; index <= i - 1; index++) {
     arr.push(arr.at(-1) + arr.at(-2))
 }
 document.write(arr.join('<br>'));
