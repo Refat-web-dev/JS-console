30b1920e18b57a73806f90afdd435a490cb874de
main <refabatya@gmail.com>
2023-03-25 00:25:25 +0500

    getMinFunction

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-let arr = [0, 1]
-let i
-do {
-    i = +prompt('Enter a num')
-} while (!Number(i));
-for (let index = 0; index <= i - 1; index++) {
-    arr.push(arr.at(-1) + arr.at(-2))
-}
-document.write(arr.join('<br>'));
+let min = (...arg) => Math.min(...(arg).flatMap(n => (n instanceof Object) ? Object.values(n) : n))
+
+console.log(min(1, 2))
+console.log(min([5, 4], [2, 3]))
+console.log(min({ a: 1, b: 2 }))
+console.log(min({ a: 1, b: 2 }, { a: 11, b: 12 })) 
