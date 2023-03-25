652eff15d9b2274546afe10d5ea189f151c058e2
main <refabatya@gmail.com>
2023-03-25 11:06:40 +0500

    sort

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-let min = (...arg) => Math.min(...(arg).flatMap(n => (n instanceof Object) ? Object.values(n) : n))
+function sortArr(arr) {
+    let len = arr.length;
+    let swapped;
 
-console.log(min(1, 2))
-console.log(min([5, 4], [2, 3]))
-console.log(min({ a: 1, b: 2 }))
-console.log(min({ a: 1, b: 2 }, { a: 11, b: 12 })) 
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+        len--;
+    } while (swapped);
+
+    return arr;
+}
+
+console.log(sortArr([5, 2, 7, 1, 9])); 
+function bubbleSort(array) {
+    let arr = Array.from(array);
+    let length = arr.length;
+    while (length != 0) {
+      for (let i = 1; i < length; i++) {
+        if (arr[i] < arr[i - 1]) {
+          [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+        }
+      }
+      length -= 1;
+    }
+    return arr;
+  }
+console.log(bubbleSort([5, 2, 7, 1, 9])); 
