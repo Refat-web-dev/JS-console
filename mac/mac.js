9d1d6e559ee1b7573be9c0cd9f77f8292000ea10
main <refabatya@gmail.com>
2023-04-13 13:02:00 +0500

    done

--- mac/mac.js
+++ mac/mac.js
@@ -1,12 +1,25 @@
 let openBtn = document.querySelector('.brg_open')
-let closeBtn = document.querySelector('.brg_close')
 let burger = document.querySelector('.burger')
 let bgrbg = document.querySelector('.bgr_bg')
-let spaceGrey = document.querySelector('.space_grey')
-let white = document.querySelector('.white')
-let priceView = document.querySelector('b')
-let addPrices = document.querySelectorAll('.space_item')
-let orgPrice = +priceView.innerHTML
+openBtn.onclick = () => {
+    burger.style.right = '0'
+    openBtn.style.display = 'none'
+    bgrbg.style.display = "block"
+    setTimeout(() => {
+        bgrbg.style.opacity = "1"
+    }, 200);
+}
+let closeBtn = document.querySelectorAll('.brg_close')
+closeBtn.forEach(btn => {
+    btn.onclick = () => {
+        burger.style.right = '-20%'
+        bgrbg.style.opacity = "0"
+        setTimeout(() => {
+            bgrbg.style.display = "none"
+            openBtn.style.display = 'flex'
+        }, 200);
+    }
+})
 const btns = document.querySelectorAll('.btns')
 const wrapImg = document.querySelector('.wrap_img')
 const wrapText = document.querySelector('.wrap_text')
@@ -18,36 +31,31 @@ const macTexts = {
     'White': "White",
     'Space Grey': "Space Grey"
 }
-openBtn.onclick = () => {
-    burger.style.right = '0'
-    openBtn.style.display = 'none'
-    bgrbg.style.display = "block"
-    setTimeout(() => {
-        bgrbg.style.opacity = "1"
-    }, 200);
-}
-closeBtn.onclick = () => {
-    burger.style.right = '-20%'
-    bgrbg.style.opacity = "0"
-    setTimeout(() => {
-        bgrbg.style.display = "none"
-        openBtn.style.display = 'flex'
-    }, 200);
-}
 btns.forEach(btn => {
     btn.onclick = () => {
         let key = btn.getAttribute('data-img')
         wrapText.innerText = `${macTexts[key]}`
         wrapImg.style.backgroundImage = `url(${macImages[key]})`
-        btn.classList.add('btn_active')
+        if (key === 'Space Grey') {
+            btns[1].classList.add("btn_active")
+            btns[0].classList.remove("btn_active")
+        } else {
+            btns[0].classList.add("btn_active")
+            btns[1].classList.remove("btn_active")
+        }
     }
-    btn.onmouseleave = () => {
-        btn.classList.remove('btn_active')
-    }
-    
+
 });
+let priceView = document.querySelector('b')
+let addPrices = document.querySelectorAll('.space_item')
+let orgPrice = priceView.innerHTML
+let split = document.querySelector(".split")
 addPrices.forEach(btn => {
     btn.onclick = () => {
+        let key = +btn.getAttribute('data-price')
+        priceView.innerText = [+orgPrice.split(',').join('') + key].join('').split('').join(' ').replace(' ', ',').replaceAll(' ','')
+        
+        console.log();
         btn.style.border = '2px solid #007FFF'
     }
     btn.onmouseleave = () => {
