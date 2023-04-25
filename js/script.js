d4f5353255c7bb9dc0a52547ea82ba3c03ab9edd
main <refabatya@gmail.com>
2023-04-25 11:58:41 +0500

    done

--- js/script.js
+++ js/script.js
@@ -1,40 +1,75 @@
-// forms
 
-let forms = document.querySelectorAll('form')
+let form = document.forms.example_form
+let inputs = form.querySelectorAll('.needed input')
+let inputes = form.querySelectorAll('input')
+let btn = form.querySelector("button")
 
-forms.forEach((form) => {
-    let inputs = form.querySelectorAll('.needed')
+let all = document.querySelector('#all')
+let need = document.querySelector('#need')
+let success = document.querySelector('#success')
+let error = document.querySelector('#error')
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
+all.innerHTML = inputes.length
+need.innerHTML = inputs.length
 
-        let allInputsFilled = true
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let allInputsFilled = true
+    blue = 0
+    red = 0
+    inputs.forEach(inp => {
 
-        inputs.forEach((input) => {
-            input.style.border = '2px solid #543FD3'
+        inp.style.border = "1px solid blue"
 
-            if (input.value.length === 0) {
-                input.style.border = '2px solid #EE0004'
-                allInputsFilled = false
+        let title = inp.previousElementSibling
+        let text = inp.nextElementSibling
+
+        if (inp.value.length === 0) {
+            btn.style.backgroundColor = 'red'
+
+            inp.style.border = '1px solid red'
+
+            title.style.color = 'red'
+
+            text.style.color = 'red'
+            text.innerHTML = "Please fiil the field"
+
+            red++
+            if (red === 7) {
+                success.innerHTML = `${0}/ ${inputs.length}`
             }
-        })
-
-        if (allInputsFilled) {
-            submit(form)
+            error.innerHTML = `${red}/ ${inputs.length}`
+            allInputsFilled = false
         } else {
-            alert('Please fill in all fields')
+
+            title.style.color = 'blue'
+
+            text.style.color = 'blue'
+            text.innerHTML = ""
+
+            btn.style.backgroundColor = 'blue'
+
+            blue++
+            if (blue === 7) {
+                error~.innerHTML = `${0}/ ${inputs.length}`
+            }
+            success.innerHTML = `${blue}/ ${inputs.length}`
         }
     })
-})
 
-function submit(form) {
+    if (allInputsFilled) {
+        submit()
+    } else {
+        alert("У вас пропущенo")
+    }
+}
+
+function submit() {
+    let fm = new FormData(form)
     let user = {}
-    let formData = new FormData(form)
-
-    formData.forEach((value, key) => {
+    fm.forEach((value, key) => {
         user[key] = value
-    })
-
+    });
     form.reset()
-    console.log(user)
+
 }
