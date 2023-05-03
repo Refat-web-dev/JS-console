9a82c01f5edca2675585a130f458f3229fcd1a76
main <refabatya@gmail.com>
2023-05-04 03:40:06 +0500

    modules

--- js/script.js
+++ js/script.js
@@ -1,105 +1,41 @@
+import arr from "../modules/db"
+let icons = ['usd.svg', 'star.svg', 'box.svg']
+let cont = document.querySelector(".container")
 
-let form = document.forms.example_form
-let inputs = form.querySelectorAll('.needed input')
-let inputes = form.querySelectorAll('input')
-let btn = form.querySelector("button")
+for (let item of arr) {
+    let item_div = document.createElement('div')
+    let img_content = document.createElement('img')
+    let decr_div = document.createElement('div')
+    let h3 = document.createElement('h3')
+    let p = document.createElement('p')
+    let row = document.createElement('div')
+    let btn = document.createElement('button')
 
-let all = document.querySelector('#all')
-let need = document.querySelector('#need')
-let success = document.querySelector('#success')
-let error = document.querySelector('#error')
+    for (let item of icons) {
+        let feature = document.createElement('div')
+        let img = document.createElement('img')
+        let span = document.createElement('span')
 
-all.innerHTML = inputes.length
-need.innerHTML = inputs.length
+        feature.classList.add('feature')
+        img.src = "./img/icons/" + item
+        span.innerHTML = "100"
 
-inputes.forEach(inp => {
-    let patterns = {
-        name: /^[a-z а-я ,.'-]+$/i,
-        mom: /^[a-z а-я ,.'-]+$/i,
-        dad: /^[a-z а-я ,.'-]+$/i,
-        surname: /^[a-z а-я ,.'-]+$/i,
-        email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-        phone: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
-        age: /^\S[0-9]{0,2}$/,
-        jS: /\w/,
-        cSS: /\w/,
-        hTML: /\w/,
-        about: /\w/,
-        car: /\w/
+        feature.append(img, span)
+        row.append(feature)
     }
-    function validate(regex, field) {
-        if (regex.test(field.value)) {
-            field.style.border = "1px solid green"
-        } else {
-            field.style.border = "1px solid red"
-        }
 
-        let greenInputs = form.querySelectorAll('.needed input[style="border: 1px solid green;"]')
-        let redInputs = form.querySelectorAll('.needed input[style="border: 1px solid red;"]')
-        success.innerHTML = `${greenInputs.length}/ ${inputs.length}`
-        error.innerHTML = `${redInputs.length}/ ${inputs.length}`
-    }
-    inp.onkeyup = () => validate(patterns[inp.name], inp)
+    item_div.classList.add('item')
+    img_content.classList.add('content')
+    decr_div.classList.add('description')
+    row.classList.add('row')
 
-})
+    h3.innerHTML = item.category
+    p.innerHTML = item.description.slice(0, 90).toLowerCase()
+    btn.innerHTML = "В избранные"
 
+    img_content.src = item.image
 
-form.onsubmit = (event) => {
-    event.preventDefault()
-    let allInputsFilled = true
-    let blue = 0
-    let red = 0
-    inputs.forEach(inp => {
-
-        inp.style.border = "1px solid blue"
-
-        let title = inp.previousElementSibling
-        let text = inp.nextElementSibling
-
-        if (inp.value.length === 0) {
-            btn.style.backgroundColor = 'red'
-
-            inp.style.border = '1px solid red'
-
-            title.style.color = 'red'
-
-            text.style.color = 'red'
-            text.innerHTML = "Please fiil the field"
-
-            red++
-            if (red === 7) {
-                success.innerHTML = `${0}/ ${inputs.length}`
-            }
-            error.innerHTML = `${red}/ ${inputs.length}`
-            allInputsFilled = false
-        } else {
-
-            title.style.color = 'blue'
-
-            text.style.color = 'blue'
-            text.innerHTML = ""
-
-
-            blue++
-            if (blue === 7) {
-                btn.style.backgroundColor = 'blue'
-                error.innerHTML = `${0}/ ${inputs.length}`
-            }
-            success.innerHTML = `${blue}/ ${inputs.length}`
-        }
-    })
-
-    if (allInputsFilled) {
-        submit()
-    }
-}
-
-function submit() {
-    let fm = new FormData(form)
-    let user = {}
-    fm.forEach((value, key) => {
-        user[key] = value
-    });
-    form.reset()
-    console.log(user);
-}
+    item_div.append(img_content, decr_div)
+    decr_div.append(h3, p, row, btn)
+    cont.append(item_div)
+}   
