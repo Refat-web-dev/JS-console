75c2284af46526a336a6ae86e7990e416e10859a
main <refabatya@gmail.com>
2023-05-08 16:00:55 +0500

    reload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import products from "../modules/db.js"
 let cont = document.querySelector(".container")
 let brg_list = document.querySelector(".burger_list")
 let total_rate = document.querySelector(".total_rate")
+let submit = total_rate.parentNode.nextElementSibling
 
 // modules
 
@@ -45,7 +46,16 @@ function reload(arr, place) {
 
         h3.innerHTML = item.category.toUpperCase()
         p.innerHTML = item.description[0].toUpperCase() + item.description.slice(1, 90).toLowerCase()
-        btn.innerHTML = "В избранные"
+
+        if (cart.includes(item.id)) {
+            btn.innerHTML = "Добавлено"
+            btn.style.background = "#1F86FF"
+            btn.style.color = "#FFF"
+        } else {
+            btn.style.background = "#FFF"
+            btn.style.color = "#000"
+            btn.innerHTML = "В избранные"
+        }
 
         img_content.src = item.image
 
@@ -57,8 +67,8 @@ function reload(arr, place) {
         btn.onclick = () => {
             brg_list.innerHTML = ""
 
-            if (cart.includes(item)) {
-                cart = cart.filter(id => id !== item)
+            if (cart.includes(item.id)) {
+                cart = cart.filter(id => id !== item.id)
                 btn.style.background = "#FFF"
                 btn.style.color = "#000"
                 btn.innerHTML = "В избранные"
@@ -66,19 +76,12 @@ function reload(arr, place) {
                 btn.style.background = "#1F86FF"
                 btn.style.color = "#FFF"
                 btn.innerHTML = "Добавлено"
-                cart.push(item)
+                cart.push(item.id)
             }
             includes.innerHTML = cart.length
 
-            if (cart.length >= 1) {
-                round.style.opacity = '1'
-                round.style.scale = '1'
-            }
-            else {
-                round.style.opacity = '0'
-                round.style.scale = '0'
-            }
-            reloadTwo(cart, brg_list)
+            reloadTwo()
+            // reloadTwo(cart, brg_list)
         }
     }
 }
@@ -119,90 +122,104 @@ closeBtn.forEach(btn => {
 })
 
 // burger module
-
-function reloadTwo(arr, place) {
+function reloadTwo() {
+    brg_list.innerHTML = ''
     let total = 0
+    for (let item of products) {
 
-    for (let el of arr) {
-        let brg_item = document.createElement('div')
-        let item_left = document.createElement('div')
-        let left_img = document.createElement('img')
-        let item_center = document.createElement('div')
-        let item_descr = document.createElement('h3')
-        let center_row = document.createElement('div')
-        let saler = document.createElement('p')
-        let counter = document.createElement('div')
-        let minus = document.createElement('button')
-        let item_span = document.createElement('span')
-        let plus = document.createElement('button')
-        let one = document.createElement('p')
-        let one_count = document.createElement('span')
-        let item_right = document.createElement('div')
-        let delet = document.createElement('button')
-        let total_price = document.createElement('span')
-
-
-        brg_item.classList.add('burger_item')
-        item_left.classList.add('item_left')
-        item_left.classList.add('item_left')
-        item_center.classList.add('item_center')
-        center_row.classList.add('center_row')
-        saler.classList.add('saler')
-        counter.classList.add('counter')
-        minus.classList.add('minus')
-        plus.classList.add('plus')
-        item_right.classList.add('item_right')
-        delet.classList.add('delete')
-        total_price.classList.add('total_price')
-
-        left_img.src = el.image
-        left_img.alt = "invalid img"
-        item_descr.innerHTML = el.category
-        saler.innerHTML = el.title.slice(0, 20)
-        item_span.innerHTML = "1"
-        one.innerHTML = " сум/ед."
-        one_count.id = "onlyOne"
-        one_count.innerHTML = el.price
-        one.style.display = 'none'
-        delet.innerHTML = "Удалить"
-        total_price.innerHTML = el.price *= item_span.innerHTML
-        brg_item.append(item_left, item_center, item_right)
-        item_left.append(left_img)
-        item_center.append(item_descr, center_row)
-        center_row.append(saler, counter)
-        counter.append(minus, item_span, plus, one)
-        one.prepend(one_count)
-        item_right.append(delet, total_price)
-        place.append(brg_item)
+        for (let el of cart) {
+            if (item.id === el) {
+                let brg_item = document.createElement('div')
+                let item_left = document.createElement('div')
+                let left_img = document.createElement('img')
+                let item_center = document.createElement('div')
+                let item_descr = document.createElement('h3')
+                let center_row = document.createElement('div')
+                let saler = document.createElement('p')
+                let counter = document.createElement('div')
+                let minus = document.createElement('button')
+                let item_span = document.createElement('span')
+                let plus = document.createElement('button')
+                let one = document.createElement('p')
+                let one_count = document.createElement('span')
+                let item_right = document.createElement('div')
+                let delet = document.createElement('button')
+                let total_price = document.createElement('span')
 
 
-        total += el.price * item_span.innerHTML
-        plus.onclick = () => {
-            total += el.price * item_span.innerHTML
-            item_span.innerHTML++
-            total_price.innerHTML = one_count.innerHTML * item_span.innerHTML
-            one.style.display = 'block'
-            total_rate.innerHTML = total.toFixed(2)
-        }
-        
-        minus.onclick = () => {
-            total -= el.price * item_span.innerHTML
-            if (item_span.innerHTML < 1) {
-                item_span.innerHTML++
+                brg_item.classList.add('burger_item')
+                item_left.classList.add('item_left')
+                item_left.classList.add('item_left')
+                item_center.classList.add('item_center')
+                center_row.classList.add('center_row')
+                saler.classList.add('saler')
+                counter.classList.add('counter')
+                minus.classList.add('minus')
+                plus.classList.add('plus')
+                item_right.classList.add('item_right')
+                delet.classList.add('delete')
+                total_price.classList.add('total_price')
+
+                left_img.src = item.image
+                left_img.alt = "invalid img"
+                item_descr.innerHTML = item.category
+                saler.innerHTML = item.title.slice(0, 20)
+                item_span.innerHTML = "1"
+                one.innerHTML = " сум/ед."
+                one_count.id = "onlyOne"
+                one_count.innerHTML = item.price
                 one.style.display = 'none'
+                delet.innerHTML = "Удалить"
+                total_price.innerHTML = item.price *= item_span.innerHTML
+
+                brg_item.append(item_left, item_center, item_right)
+                item_left.append(left_img)
+                item_center.append(item_descr, center_row)
+                center_row.append(saler, counter)
+                counter.append(minus, item_span, plus, one)
+                one.prepend(one_count)
+                item_right.append(delet, total_price)
+                brg_list.append(brg_item)
+
+
+                total += item.price
+                plus.onclick = () => {
+                    total += item.price
+                    item_span.innerHTML++
+                    total_price.innerHTML = one_count.innerHTML * item_span.innerHTML
+                    one.style.display = 'block'
+                    total_rate.innerHTML = total.toLocaleString("uK-UK")
+                }
+
+                minus.onclick = () => {
+                    total -= item.price
+                    if (item_span.innerHTML === 1) {
+                        item_span.innerHTML++
+                        one.style.display = 'none'
+                    }
+                    item_span.innerHTML--
+                    total_price.innerHTML = one_count.innerHTML * item_span.innerHTML
+                    one.style.display = 'block'
+                    total_rate.innerHTML = total.toFixed(2)
+                }
+
+                delet.onclick = () => {
+                    cart = cart.filter(id => item.id !== id)
+                    total_rate.innerHTML = (total - total_price.innerHTML).toLocaleString("uK-UK")
+                    reloadTwo()
+                    reload(products, cont)
+                }
+                total_rate.innerHTML = total.toLocaleString("uK-UK")
             }
-            item_span.innerHTML--
-            total_price.innerHTML = one_count.innerHTML * item_span.innerHTML
-            one.style.display = 'block'
-            total_rate.innerHTML = total.toFixed(2)
         }
-
-        delet.onclick = () => {
-            delet.parentNode.parentNode.remove()
-            total_rate.innerHTML -= total_price.innerHTML
-
-        }
-        total_rate.innerHTML = total.toFixed(2)
-
+    }
+    
+    if (cart.length >= 1) {
+        round.style.opacity = '1'
+        round.style.scale = '1'
+    }
+    else {
+        round.style.opacity = '0'
+        round.style.scale = '0'
     }
 }
